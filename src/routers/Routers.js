273844494d7feaf0d60467b0ab219150b5b1b529
unknown <Arya Karimi>
2023-12-07 13:58:52 +0300

    router added

---
+++ src/routers/Routers.js
@@ -0,0 +1,13 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+
+function Routers() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      {/* <Route path="home" element={<Home />} /> */}
+    </Routes>
+  );
+}
+
+export default Routers;
