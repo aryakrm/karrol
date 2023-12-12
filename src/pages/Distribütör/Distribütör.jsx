8af3fc28e2c263754a58724701fdb6811f15cfca
unknown <Arya Karimi>
2023-12-12 20:19:09 +0300

    dist added

--- src/pages/Distribütör/Distribütör.jsx
+++ src/pages/Distribütör/Distribütör.jsx
@@ -3,9 +3,16 @@ import "./Distribütör.scss"
 
 function Distribütör() {
   return (
-    <div>
-      Distribütor
-    </div>
+    <section className='Distributor' >
+       <img src="/assets/dist.jpg" alt="Karrol" />
+      <div className='distText' >
+        <h1>DİSTRİBÜTÖR</h1>
+      <p>ISACO
+Karrol Otomotiv, ISACO'nun Türkiye ve Kuzey Afrika'nın resmi distribütörüdür. ISACO, 50 yıllık bir geçmişe sahip, en büyük yedek parça ve otomobil üreticilerinden biridir, ve aynı zamanda IRAN KHODRO'nun yedek parça üretim ve satış sonrası tedarikini yapmaktadır. IRAN KHODRO, Orta Doğu'nun en büyük araba üreticilerinden biri olup, Peugeot ve Citroen'in ticari ortağıdır. Bu firma, Peugeot 206, 206 Hatchback ve Sedan, 206 plus, SAMAND, DENA, DENA plus ve RANA gibi çokça tercih edilen model arabaları üretmektedir.
+
+Ayrıca, AZERBAYCAN'da kendi üretim hattında XAZAR adlı arabayı da üretmekte ve IRAK, VENEZUELLA, SENEGAL'de de üretim hatları bulunmaktadır. Geçmiş yıllarda birçok arabayı IRAK, RUSYA, BELARUS, TÜRKİYE vb. ülkelere de ihraç etmiştir. Karrol Otomotiv, ISACO'nun resmi distribütörü olarak, IRAN KHODRO'nun ürettiği tüm modellerin motor ve kaporta aksamlarını tedarik etmektedir. Örnek olarak, bu firmanın ürettiği 206 ve 206+'ın ön farları ve arka stopları Türkiye pazarında bulunan en kaliteli ürünlerden biridir ve Avrupa standartlarının tamamına sahiptir.</p>
+</div>
+    </section>
   )
 }
 
