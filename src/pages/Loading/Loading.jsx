6ebdec5c887cc1d63cb0d59fb13db3b0782d7f66
unknown <Arya Karimi>
2023-12-19 20:40:42 +0300

    loading page added

--- src/pages/Loading/Loading.jsx
+++ src/pages/Loading/Loading.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import {useEffect, useState} from "react";
 import "./Loading.scss";
 import ReactLoading from "react-loading";
 
 function Loading() {
-    
+
+    const [show, setShow] = useState(false)
+  useEffect(() => {
+    let timeout = setTimeout(() => setShow(true), 5000)
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [])
+
   return (
     <div className="Loading">
-      <ReactLoading
+        {show &&  <ReactLoading
         type="spinningBubbles"
         color="#061022"
         height={667}
         width={375}
         delay={5000}
-      />
+      />}
+      
     </div>
   );
 }
